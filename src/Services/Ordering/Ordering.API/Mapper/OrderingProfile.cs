8b04feea5dc8449edb9ad35cbaa2ee07def0db24
Umesh Chauda <umeshgupta821@gmail.com>
2022-09-08 13:39:18 +0530

    added required nuget packages

---
+++ src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventBus.Messsages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.API.Mapper
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
